ca3a5bb7b5fc3f0f976e53c68acb00ea6019eaaf
Shreyansh pandey <pandeyshreyansh226@gmail.com>
2025-07-08 11:39:32 +0530

    Flood_Alert.py

--- Flood_Alert.py
+++ Flood_Alert.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 import random
-streamlit run file_name.pyimport folium
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import (
